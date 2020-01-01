a9af55d6b87b986d967e8751dc33882308b33c86
jakel181 <17256688+jakel181@users.noreply.github.com>
2020-01-01 14:19:29 -0500

    Create control.php

---
+++ control.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+<script src="code.jquery.com/jquery-3.4.1.min.js">
+<script>
+$(document).ready(functio
+<button type="button" id="ON">ON</button>
+<button type="button" id="OFF">OFF</button>
